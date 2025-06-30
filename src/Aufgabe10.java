fa0eaccd35f960ac9d050db5de5263f66fe95f90
Philip Rhinow <philipprhinowgmail.com>
2025-06-30 21:42:55 +0200

    commit

---
+++ src/Aufgabe10.java
@@ -0,0 +1,14 @@
+public class Aufgabe10 {
+    public static void main(String[] args) {
+
+        int[] array = {4, 8, 2, 12, 9, 32, 65, 28, 59, 17, 9};
+
+
+    }
+    static void minimum(int[]) {
+        boolean sorted = false;
+        while (!sorted) {
+
+        }
+    }
+}
