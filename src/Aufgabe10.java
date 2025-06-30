e4a3da4edfa95960ef64e57c977c817c486cec81
Philip Rhinow <philipprhinowgmail.com>
2025-06-30 23:20:21 +0200

    commit

--- src/Aufgabe10.java
+++ src/Aufgabe10.java
@@ -3,12 +3,29 @@ public class Aufgabe10 {
 
         int[] array = {4, 8, 2, 12, 9, 32, 65, 28, 59, 17, 9};
 
-
+        System.out.println("Maximum = " + Maximum(array));
+        System.out.println("Minimum = " + Minimum(array));
     }
-    static void minimum(int[]) {
-        boolean sorted = false;
-        while (!sorted) {
+    static int Maximum(int[] array) {
 
+        int arraylength = array.length;
+        int temp = array[0];
+        for (int i = 0; i < arraylength; i++) {
+            if (temp < array[i]) {
+                temp = array[i];
+            }
         }
+        return temp;
+    }
+    static int Minimum(int[] array) {
+
+        int arraylength = array.length;
+        int temp = array[0];
+        for (int i = 0; i < arraylength; i++) {
+            if (temp > array[i]) {
+                temp = array[i];
+            }
+        }
+        return temp;
     }
 }
