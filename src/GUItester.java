7e32949878dcf6fb7a4abb2361b0f54e23c64f59
Philip Rhinow <philipprhinowgmail.com>
2025-07-08 19:29:19 +0200

    commit

--- src/GUItester.java
+++ src/GUItester.java
@@ -2,8 +2,6 @@ public class GUItester {
 
     public static void main(String[] args)
     {
-        GUI gd = new GUI(640,480);
-        gd.setupGUI();
-        gd.setUpButtonListeners();
+
     }
 }
