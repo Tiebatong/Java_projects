3d8d281396cf1b2db2c4dc5051d49ec96baf9422
Philip Rhinow <philipprhinow@gmail.com>
2025-07-09 22:03:29 +0200

    commit

--- src/Aufgabe15.java
+++ src/Aufgabe15.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Aufgabe15 {
 
     public static void main(String[] args) {
-        int[] primzahlen = {2, 3, 5, 7, 11, 13,17, 19, 23, 29, 31, 37, 41, 43, 47,};
+        int[] primzahlen = {2, 3, 5, 7, 11, 13,17, 19, 23, 29, 31, 37, 41, 43, 47};
 
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Bitte geben sie eine Zahl zwischen 2 und 100 ein:");
+        int eingabe = scan.nextInt();
+        boolean weiter = false;
+        while(!weiter) {
+            for(int i = 0; i < primzahlen.length; i++) {
+                 int temp = eingabe / primzahlen[i];
+            }
+        }
     }
 }
