e86e452641f6aea08424f0990150bcef6541a508
Philip Rhinow <philipprhinow@gmail.com>
2025-07-11 23:05:58 +0200

    commit

---
+++ src/Fibonaci.java
@@ -0,0 +1,17 @@
+public class Fibonaci {
+    public static void main(String[] args) {
+
+        int Iterationen = 50;
+        long fib1 = 1;
+        long fib2 = 1;
+        long temp = 0;
+
+        for(int i = 0; i < Iterationen; i++) {
+            temp = fib2;
+            fib2 = fib1 + fib2;
+            fib1 = temp;
+        }
+        System.out.println(fib1);
+
+    }
+}
