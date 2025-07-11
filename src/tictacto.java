7838344043981422a808f88f406e3e245e088ccf
Philip Rhinow <philipprhinow@gmail.com>
2025-07-11 23:34:25 +0200

    commit

--- src/tictacto.java
+++ src/tictacto.java
@@ -1,37 +1,9 @@
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
-public class tictacto implements ActionListener{
+public class tictacto {
 
-    JFrame frame;
-    JTextField textField;
-    JButton[] Spielfeld = new JButton[9];
-    JButton Spielfeld1, Spielfeld2 , Spielfeld3 , Spielfeld4 , Spielfeld5 , Spielfeld6 , Spielfeld7 , Spielfeld8 , Spielfeld9;
+    public static void main(String[] args ) {
 
-    tictacto() {
-
-        frame = new JFrame("Calculator");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(420, 550);
-        frame.setLayout(null);
-        JPanel panel = new JPanel();
-
-        Spielfeld1 = new JButton("test");
-
-        panel.add(Spielfeld1);
-
-        frame.setVisible(true);
-
+        JFrame frame = new JFrame();
     }
-    public static void main(String[] args) {
-
-        tictacto tic = new tictacto();
-
-    }
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
-
 }
