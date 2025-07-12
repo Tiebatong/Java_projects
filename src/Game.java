019c83c0fd4940bd420c1a27a37b1eed22665c7f
Philip Rhinow <philipprhinow@gmail.com>
2025-07-12 22:04:48 +0200

    commit

--- src/Game.java
+++ src/Game.java
@@ -7,9 +7,14 @@ import java.util.Random;
 public class Game implements ActionListener {
 
     JButton[] buttons;
+    JTextField text;
     boolean Spieler_Zug = true;
     boolean[] Feld_besetzt = {false, false, false, false, false, false, false, false, false};
-    char[] Spielfeld = {};
+    char[] Spielfeld = {'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a', 'a'};
+    boolean Computer_gewonnen = false;
+    boolean Spieler_gewonnen = false;
+    int Zuege_Count = 0;
+
 
 
 
@@ -43,15 +48,51 @@ public class Game implements ActionListener {
         buttons[8].setBounds(220, 220, 50, 50);
 
 
+        text = new JTextField();
+        text.setBounds(140, 20, 100, 50);
+        text.setEditable(false);
+        text.setBackground(Color.LIGHT_GRAY);
+
+
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(380, 400);
         frame.setLayout(null);
-        frame.setVisible(true);
         frame.getContentPane().setBackground(Color.darkGray);
+        frame.add(text);
         frame.setResizable(false);
+        frame.setVisible(true);
+
 
     }
+    public int Check_gewonnen(char[] Spielfeld, int Spieler) {
+
+        char z = 'X';          // int Spieler = 1 heisst Spieler
+        if(Spieler == 2) {     // int Spieler = 2 heisst Computer
+            z = 'O';           // return 3 heisst keiner hat gewonnen
+        }
+
+        if(Spielfeld[0] == z && Spielfeld[1] == z && Spielfeld[2] == z) {
+            return Spieler;
+        } else if (Spielfeld[3] == z && Spielfeld[4] == z && Spielfeld[5] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[6] == z && Spielfeld[7] == z && Spielfeld[8] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[0] == z && Spielfeld[3] == z && Spielfeld[6] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[1] == z && Spielfeld[4] == z && Spielfeld[7] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[2] == z && Spielfeld[5] == z && Spielfeld[8] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[0] == z && Spielfeld[4] == z && Spielfeld[8] == z) {
+            return  Spieler;
+        }  else if (Spielfeld[2] == z && Spielfeld[4] == z && Spielfeld[6] == z) {
+            return  Spieler;
+        } else {
+            return 3;
+        }
+    }
+
 
     public static void main(String[] args ) {
         new Game();
@@ -59,39 +100,63 @@ public class Game implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        int Computer_Zug = 0;
-        Random rand = new Random();
-        boolean valider_Zug = true;
-        while(valider_Zug) {
-            Computer_Zug = rand.nextInt(9);
+
+        if(Spieler_Zug) {
             for(int i = 0; i < 9; i++) {
-                if(!Feld_besetzt[Computer_Zug]) {
-                    valider_Zug = false;
-                }
+                if(e.getSource() == buttons[i] && !Feld_besetzt[i]) {
+                    buttons[i].setText("X");
+                    Spielfeld[i] = 'X';
+                    Spieler_Zug = false;
+                    Feld_besetzt[i] = true;
+                    Zuege_Count++;
 
-            }
+                    if(Check_gewonnen(Spielfeld, 1) == 1) {
+                        text.setText("GEWONNEN");
 
-        }
+                    }  else if (Zuege_Count == 9 && Check_gewonnen(Spielfeld, 1) == 3) {
+                        text.setText("Unentschieden");
 
-
-
-        for(int x = 0; x < 9; x++) {
-            if(Spieler_Zug) {
-                for(int i = 0; i < 9; i++) {
-                    if(e.getSource() == buttons[i]) {
-                        buttons[i].setText("X");
-                        Spieler_Zug = false;
-                        Feld_besetzt[i] = true;
-                        break;
                     }
-                }
-            } else {
 
-                buttons[Computer_Zug].setText("O");
-                Spieler_Zug = true;
-                Feld_besetzt[Computer_Zug] = true;
+                    break;
+
+                }
             }
+
         }
+
+        if(!Spieler_Zug) {
+            int Computer_Zug = 0;
+            Random rand = new Random();
+
+            do {
+                Computer_Zug = rand.nextInt(9);
+            } while (Feld_besetzt[Computer_Zug]);
+
+            buttons[Computer_Zug].setText("O");
+            Spielfeld[Computer_Zug] = 'O';
+            Spieler_Zug = true;
+            Feld_besetzt[Computer_Zug] = true;
+            Zuege_Count++;
+
+            if (Check_gewonnen(Spielfeld, 2) == 2) {
+                text.setText("VERLOREN");
+            } else if (Zuege_Count == 9 && Check_gewonnen(Spielfeld, 2) == 3) {
+                text.setText("Unentschieden");
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
 
 
 
