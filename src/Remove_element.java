63858e4d6b489e1a75d3ede4bb26357922bb0cf4
Philip Rhinow <philipprhinow@gmail.com>
2025-08-07 20:26:16 +0200

    Commit Leetcode

---
+++ src/Remove_element.java
@@ -0,0 +1,28 @@
+public class Remove_element {
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 2, 3, 6, 8, 3, 6, 9, 3 ,3};
+        int val = 3;
+        int counter = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                nums[i] = 0;
+            } else {
+                nums[counter] = nums[i];
+                counter++;
+            }
+        }
+
+        for (int x = 0; x < (nums.length - counter); x++) {
+            nums[counter + x] = 0;
+        }
+        int k = counter;
+
+        System.out.println(k);
+
+        for (int j = 0; j < nums.length; j++) {
+            System.out.print(nums[j]);
+        }
+    }
+}
