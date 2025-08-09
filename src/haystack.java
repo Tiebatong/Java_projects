7102f8a11fcba41af9bd26917246eb20aa04fdc7
Philip Rhinow <philipprhinow@gmail.com>
2025-08-09 15:13:04 +0200

    commit

---
+++ src/haystack.java
@@ -0,0 +1,15 @@
+public class haystack {
+    public static void main(String[] args) {
+        String haystack = "abc";
+        String needle = "c";
+        String substring;
+
+        for ( int i = 0; i <= haystack.length() - needle.length(); i++) {
+            substring = haystack.substring(i, i + needle.length());
+            if ( substring.equals(needle)) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
