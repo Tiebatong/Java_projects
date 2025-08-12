725f66ec6f14750e50682450c697328ebe23d270
Philip Rhinow <philipprhinow@gmail.com>
2025-08-13 00:02:37 +0200

    commit

--- src/Leetcode_26.java
+++ src/Leetcode_26.java
@@ -1,7 +1,7 @@
 public class Leetcode_26 {
     public static void main(String[] args) {
 
-        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int[] nums = {0,0,1,1,1,2,2,3,3,4,5};
         int k = 1;
 
         if (nums.length == 0) {
@@ -15,11 +15,15 @@ public class Leetcode_26 {
             }
         }
 
+        for (int i = 1; i < (nums.length - k) + 1; i++) {
+            nums[nums.length - (1 * i)] = 0;
+        }
 
         for (int j = 0; j < nums.length; j++) {
             System.out.print(nums[j]);
         }
-        System.out.println();
+
+        System.out.println(" ");
         System.out.println(k);
 
 
