a31dcca84ad3f9a482f18af6ef83ec8e3a3a4edb
Philip Rhinow <philipprhinow@gmail.com>
2025-08-15 01:19:36 +0200

    commit

--- src/Leetcode_2264.java
+++ src/Leetcode_2264.java
@@ -1,11 +1,9 @@
-import java.util.ArrayList;
-
 public class Leetcode_2264 {
     public static void main(String[] args) {
 
         String num = "11234534576";
         String solution = "";
-        String substring = num.substring(0, 3);
+        String substring;
         int i = 0;
         do {
             substring = num.substring(i, i + 3);
