deb6a83eaf7091de97478c6a630c0793b776fa53
Philip Rhinow <philipprhinow@gmail.com>
2025-08-14 22:53:47 +0200

    commit

--- src/Leetcode_2264.java
+++ src/Leetcode_2264.java
@@ -3,40 +3,24 @@ import java.util.ArrayList;
 public class Leetcode_2264 {
     public static void main(String[] args) {
 
-        String num = "12221000333";
-        ArrayList<String> list = new ArrayList<>();
+        String num = "11234534576";
+        String solution = "";
         String substring = num.substring(0, 3);
         int i = 0;
         do {
             substring = num.substring(i, i + 3);
             if (substring.charAt(0) == substring.charAt(1) && substring.charAt(1) == substring.charAt(2)) {
 
-                if (list.isEmpty()) {
-                    list.add(substring);
-                } else if (Integer.valueOf(list.getFirst()) < Integer.valueOf(substring)) {
-                    list.remove(0);
-                    list.add(substring);
+                if (solution == "") {
+                    solution = substring;
+                } else if (Integer.valueOf(solution) < Integer.valueOf(substring)) {
+                    solution = substring;
                 }
 
             }
             i++;
         } while (i < num.length() - 2);
 
-        /*
-                if (list.isEmpty()) {
-            System.out.println("");;
-        } else {
-            solution = list.getFirst();
-            for (int j = 1; j < list.size(); j++) {
-                if(Integer.valueOf(solution) <  Integer.valueOf(list.get(j))) {
-                    solution = list.get(j);
-                }
-            }
-        }
-         */
-        //String solution = "";
-
-
-        System.out.println(list);
+        System.out.println(solution);
     }
 }
