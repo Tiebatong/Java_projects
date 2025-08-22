95756d14d9735cc7f12bf49905fdf1df75d17787
Philip Rhinow <philipprhinow@gmail.com>
2025-08-22 15:53:14 +0200

    Commit Leetcode

--- src/Leetcode_3195.java
+++ src/Leetcode_3195.java
@@ -1,33 +1,82 @@
 public class Leetcode_3195 {
     public static void main(String[] args) {
         int[][] grid = {
-                {1,0,1},
-                {0,1,0}
+                {0,1,0},
+                {0,0,1}
         };
 
-        int left = 9999;
-        int right = 0;
-        int top = 9999;
-        int bottom = 0;
 
-        for (int i = 0; i < grid.length; i++) { //Höhe
-            for (int j = 0; j < grid[0].length; j++) { // Breite
-                if (grid[i][j] == 1 && j <= left) {
-                    left = j;
-                }
-                if (grid[i][j] == 1 && j >= right) {
-                    right = j;
-                }
-                if (grid[i][j] == 1 && i <= top) {
+        int left = 0;
+        int right = 0;
+        int top = 0;
+        int bottom = 0;
+        boolean bool = true;
+
+        for (int i = 0; i < grid.length; i++) {
+            if (!bool){
+                break;
+            }
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
                     top = i;
-                }
-                if (grid[i][j] == 1 && i >= bottom) {
-                    bottom = i;
+                    bool = false;
+                    break;
+
                 }
             }
         }
-        right++;
-        bottom++;
+        bool = true;
+
+
+        for (int i = grid.length - 1; i >= 0; i--) {
+            if (!bool){
+                break;
+            }
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    bottom = i + 1;
+                    bool = false;
+                    break;
+
+                }
+            }
+        }
+        bool = true;
+
+        for (int j = 0; j < grid[0].length; j++) {
+            if (!bool){
+                break;
+            }
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i][j] == 1) {
+                    left = j;
+                    bool = false;
+                    break;
+
+                }
+            }
+        }
+        bool = true;
+
+        for (int j = grid[0].length - 1; j >= 0; j--) {
+            if (!bool){
+                break;
+            }
+            for (int i = 0; i < grid.length; i++) {
+                if (grid[i][j] == 1) {
+                    right = j + 1;
+                    bool = false;
+                    break;
+
+                }
+            }
+        }
+        bool = true;
+
+
+
+
+
         int x = right - left;
         int y = bottom - top;
         int z = x * y;
