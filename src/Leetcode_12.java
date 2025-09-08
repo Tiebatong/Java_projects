0e7fef46224797a3c789d2195936f446689cc020
Philip Rhinow <philipprhinow@gmail.com>
2025-09-08 22:04:46 +0200

    commit

---
+++ src/Leetcode_12.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Leetcode_12 {
+    public static void main(String[] args) {
+
+        int num = 3749;
+        int number_roman = 7;
+        int samllest = 4000;
+
+        StringBuilder sb = new StringBuilder();
+
+        int[] values = {1,5,10,50,100,500,1000};
+        char[] roman = {'I','V','X','L','C','D','M'};
+
+    }
+}
---
+++ src/Leetcode_1317.java
@@ -0,0 +1,14 @@
+public class Leetcode_1317 {
+    public static void main(String[] args) {
+
+        int n = 11;
+
+        int[] no_zero_integers = new int[2];
+
+        int a = n / 2;
+        int b = n - a;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
