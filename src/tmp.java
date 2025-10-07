e79e8a9887dbd0141716b5df4d38fd6f918d0bc3
Philip Rhinow <philipprhinow@gmail.com>
2025-10-07 16:03:46 +0200

    commit

--- src/tmp.java
+++ src/tmp.java
@@ -3,13 +3,9 @@ import java.util.LinkedList;
 public class tmp {
     public static void main(String[] args) {
 
-    int a =  239;
-    int digit;
-    while (a >= 0) {
-        digit = a % 10;
-        System.out.println(digit);
-        a /= 10;
-    }
+    int a = 100;
+    float b = a / 2;
+        System.out.println(b);
 
 
     }
