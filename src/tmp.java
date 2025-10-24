86d4deae70f11e21a5887ed179b622e60b4610ad
Philip Rhinow <philipprhinow@gmail.com>
2025-10-24 17:52:31 +0200

    Progra Vorlesung Mitschrift

--- src/tmp.java
+++ src/tmp.java
@@ -1,13 +1,15 @@
-import java.util.LinkedList;
-
 public class tmp {
     public static void main(String[] args) {
 
 
-        for (int i = 10; i > 0; i--) {
-            System.out.println("Zeile " + i);
+
+        for (int x = -10; x <= 10; x++) {
+            System.out.println("f(" + x + ") = " + f(x));
         }
 
+    }
 
+    public static double f(int x) {
+        return 5 + Math.sin(x);
     }
 }
