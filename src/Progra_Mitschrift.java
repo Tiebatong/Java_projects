049d1bf3ecead702e655432af1f0ba25a0f9d2b9
Philip Rhinow <philipprhinow@gmail.com>
2025-11-10 09:44:57 +0100

    Commit Vorlesung Mitschrift 10.11.2025

--- src/Progra_Mitschrift.java
+++ src/Progra_Mitschrift.java
@@ -2,6 +2,14 @@
 public class Progra_Mitschrift {
     public static void main(String[] args) {
 
+        point2D p1 = new point2D(0.9, 3);
+
+        point2D p2 = new point2D();
+        p2.x = 0.3;
+        p2.y = 07;
+
+        p1.subtract(p2);
+        p1.print();
 
     }
 }
--- src/tmp.java
+++ src/tmp.java
@@ -1,16 +1,11 @@
 public class tmp {
     public static void main(String[] args) {
 
-        System.out.println(fib(5));
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+
+        System.out.println(a + b);
 
     }
-    static int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        if (n == 1) {
-            return 1;
-        }
-        return fib(n - 1) + fib(n - 2);
-    }
+
 }
