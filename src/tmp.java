a7735edb7a2f1b6c18be59110b0c2fe7ab7f294f
Philip Rhinow <philipprhinow@gmail.com>
2025-11-29 22:06:10 +0100

    factorio assambler output neu formatiert

--- src/tmp.java
+++ src/tmp.java
@@ -1,15 +1,13 @@
 public class tmp {
     public static void main(String[] args) {
-        int x = 1;
-        int total = 1;
-        for (int i = 0; i < 15; i++) {
-            x *= 2;
-            total += x;
-        }
-        System.out.println(x);
-        System.out.println(x + x - 1);
-        System.out.println(total);
 
+        int exp = 1;
+        for( int i = 0; i < 10; i++) {
+
+            double result = Math.pow(2, -exp);
+            System.out.println(result);
+            exp++;
+        }
     }
 
 }
