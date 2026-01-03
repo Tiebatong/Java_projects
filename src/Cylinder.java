516a22adc9f7d5eae5ff2299066175a72952f475
Philip Rhinow <Philipprhinow@gmail.com>
2026-01-03 23:13:32 +0100

    Progra Blatt 10 A1

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -4,4 +4,57 @@ record Cylinder(double r, double h) {
     double volume() {
         return Math.PI * r * r * h;
     }
+
+    static Cylinder[] sorted (Cylinder[] CylinderArr) {
+        if (CylinderArr == null) {
+            throw new NullPointerException("Array is null");
+        }
+
+        int legnth = CylinderArr.length;
+        Cylinder[] sortedArray = CylinderArr.clone();
+
+        for (int i = 1; i < legnth; i++) {
+
+            int currentIndex = i;
+
+            while (sortedArray[currentIndex].volume() < sortedArray[currentIndex-1].volume() && currentIndex >= 1) {
+                Cylinder tmp = sortedArray[currentIndex];
+                sortedArray[currentIndex] = sortedArray[currentIndex-1];
+                sortedArray[currentIndex-1] = tmp;
+                if (currentIndex > 1) {
+                    currentIndex--;
+                }
+
+            }
+        }
+
+        return  sortedArray;
+    }
+
+    public static void main(String[] args) {
+
+
+        Cylinder c0 = new Cylinder(13, 1);
+        Cylinder c1 = new Cylinder(29, 7.5);
+        Cylinder c2 = new Cylinder(5.2, 7);
+        Cylinder c3 = new Cylinder(12.6, 9);
+        Cylinder c4 = new Cylinder(4, 1);
+        Cylinder c5 = new Cylinder(23.9, 1);
+        Cylinder c6 = new Cylinder(7, 1.6);
+
+        Cylinder[] CylinderArray = {c0,c1,c2,c3,c4,c5,c6};
+
+        for(Cylinder c: CylinderArray) {
+            System.out.println(c.volume());
+        }
+
+        Cylinder[] sortedArray = sorted(CylinderArray);
+
+        System.out.println("===================");
+
+        for(Cylinder c: sortedArray) {
+            System.out.println(c.volume());
+        }
+
+    }
 }
--- src/tmp.java
+++ src/tmp.java
@@ -1,10 +1,29 @@
 public class tmp {
     public static void main(String[] args) {
 
+        int[] intarray = {9,1,4,7,3,8,3,5,2,};
 
-        Cylinder c0 = new Cylinder(1, 1);
+        int legnth = intarray.length;
 
-        System.out.println(c0.volume());
+        for (int i = 1; i < legnth; i++) {
+
+            int currentIndex = i;
+
+            while (intarray[currentIndex] < intarray[currentIndex-1] && currentIndex >= 1) {
+                int tmp = intarray[currentIndex];
+                intarray[currentIndex] = intarray[currentIndex-1];
+                intarray[currentIndex-1] = tmp;
+                if (currentIndex > 1) {
+                    currentIndex--;
+                }
+            }
+        }
+
+        for(int i: intarray) {
+            System.out.println(i);
+        }
+
+
 
     }
 
