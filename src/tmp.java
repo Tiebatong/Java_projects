fc9d1049b63ff35901db95ce32e20c1f5874626c
Philip Rhinow <Philipprhinow@gmail.com>
2026-01-24 15:30:07 +0100

    Progra HashSet 2. Teil

--- src/tmp.java
+++ src/tmp.java
@@ -2,17 +2,21 @@
 public class tmp {
     public static void main() {
 
-        Hash_Set set = new Hash_Set();
-        System.out.println(set);
-        Person p0 = new Person("David", "Mustermann");
-        set.insert(p0);
-        System.out.println(set);
-        Person p1 = new Person("David", "Magnusen");
-        set.insert(p1);
-        System.out.println(set);
-        //set.delete(p0);
-        System.out.println(set.contains(p1));
+//        Hash_Set set = new Hash_Set();
+//        System.out.println(set);
+//        Person p0 = new Person("David", "Mustermann");
+//        set.insert(p0);
+//        System.out.println(set);
+//        Person p1 = new Person("David", "Magnusen");
+//        set.insert(p1);
+//        System.out.println(set);
+//        //set.delete(p0);
+//        System.out.println(set.contains(p1));
 
-
+        String tmp = "";
+        for (int i = 0; i < 32; i++) {
+            tmp += "1,";
+        }
+        System.out.println(tmp);
     }
 }
