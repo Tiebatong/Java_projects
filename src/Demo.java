5629069bed34d20b47c9323dc87841c25f33cbdd
Philip Rhinow <Philipprhinow@gmail.com>
2026-01-25 21:31:52 +0100

    Progra HashSet 2. Teil

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+static void main() {
+
+    Map<String, Integer> haeufigkeit = new HashMap<>();
+
+    haeufigkeit.put("Ampel", 5);
+    System.out.println(haeufigkeit);
+}
