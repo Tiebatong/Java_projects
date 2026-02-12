d83af321af36c28c3064c6418979145ae7757ef6
Philip Rhinow <Philipprhinow@gmail.com>
2026-02-12 22:26:39 +0100

    Progra Blatt 03 pascalsches Dreieck

--- src/PascalTriangle.java
+++ src/PascalTriangle.java
@@ -1,13 +1,23 @@
 public class PascalTriangle {
     static void main() {
-        System.out.println(p(3, 2));
+        pascalTriangle(5);
     }
 
     static int p(int z, int s) {
-        if (s > 0 && z > 0) {
-            return p(z - 1, s - 1) + p(z - 1, s);
-        } else {
+        if (s == 0 || s == z) {
             return 1;
+        } else {
+            return p(z - 1, s - 1) + p(z - 1, s);
+        }
+    }
+
+    static void pascalTriangle(int n) {
+
+        for (int i = 1; i < n + 2; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(p(i - 1, j) + " ");
+            }
+            System.out.println();
         }
     }
 }
