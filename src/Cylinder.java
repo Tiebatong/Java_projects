f8ec5a4aea52c33cad2f07b67b50f1e5856b23a7
Philip Rhinow <philipprhinow@gmail.com>
2026-02-18 16:23:47 +0100

    Blatt10

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,14 +1,85 @@
-record Cylinder(double radius, double heihgt) {
+record Cylinder(double r, double h) {
+
 
     double volume() {
-        return Math.PI * (this.radius * this.radius) * this.heihgt;
-
+        return Math.PI * (this.r * this.r) * this.h;
     }
 
-//    static Cylinder[] sorted(Cylinder[] unsorted) {
-//        Cylinder[] sorted = new Cylinder[unsorted.length];
-//
-//
-//
-//    }
+    static Cylinder[] sorted(Cylinder[] array) {
+        java.util.Objects.requireNonNull(array);
+        return sort(array);
+    }
+
+    static Cylinder[] sort(Cylinder[] cylinders) {
+
+
+
+
+        for (Cylinder c: cylinders) {
+            java.util.Objects.requireNonNull(c);
+        }
+        if (cylinders.length == 1) {
+            return cylinders;
+        }
+        Cylinder[] A = first_half(cylinders);
+        Cylinder[] B = secend_half(cylinders);
+
+        Cylinder[] arrA = sort(A);
+        Cylinder[] arrB = sort(B);
+
+        return merge(arrA, arrB);
+    }
+
+    static Cylinder[] first_half(Cylinder[] array) {
+        int length = array.length / 2;
+
+        Cylinder[] halfed = new Cylinder[length];
+        for (int i = 0; i < length; i++) {
+            halfed[i] = array[i];
+        }
+        return halfed;
+    }
+
+    static Cylinder[] secend_half(Cylinder[] array) {
+        int length = array.length - array.length / 2;
+
+        Cylinder[] halfed = new Cylinder[length];
+        for (int i = 0; i < length; i++) {
+            halfed[i] = array[i + array.length / 2];
+        }
+        return halfed;
+    }
+
+    static Cylinder[] merge(Cylinder[] A, Cylinder[] B) {
+
+        int index_A = 0;
+        int index_B = 0;
+        int index_merged = 0;
+
+        Cylinder[] merged = new Cylinder[A.length + B.length];
+
+        while ( index_A < A.length || index_B < B.length) {
+
+            if (index_A >= A.length) {
+                merged[index_merged] = B[index_B];
+                index_merged++;
+                index_B++;
+            } else if (index_B >= B.length) {
+                merged[index_merged] = A[index_A];
+                index_merged++;
+                index_A++;
+            } else if (A[index_A].volume() < B[index_B].volume()) {
+                merged[index_merged] = A[index_A];
+                index_merged++;
+                index_A++;
+            } else {
+                merged[index_merged] = B[index_B];
+                index_merged++;
+                index_B++;
+            }
+        }
+
+        return merged;
+    }
+
 }
--- src/tmp.java
+++ src/tmp.java
@@ -2,23 +2,56 @@
 public class tmp {
     public static void main() {
 
-    int[] a0 = {2, 0, 5};
-    int[] a1 = {-1, 2, 8};
+        Cylinder c0 = new Cylinder(2, 2);
+        Cylinder c1 = new Cylinder(3, 6);
+        Cylinder c2 = new Cylinder(7, 12);
+        Cylinder c3 = new Cylinder(3, 55);
+        Cylinder c4 = new Cylinder(6, 7);
+        Cylinder c5 = new Cylinder(2, 8);
+        Cylinder c6 = new Cylinder(8, 89);
+        Cylinder c7 = new Cylinder(12, 1);
 
-    for (int i = 0; i < a0.length; i++) {
-        for (int j = 0; j < a1.length; j++) {
-            System.out.print(a0[i] + a1[j] + ", ");
-        }
-    }
+        Cylinder[] Carr = {};
 
-    }
-
-    public static int fac(int x) {
-        if (x > 1) {
-            return x * fac(x - 1);
-        } else {
-            return 1;
+        Cylinder[] sorted = Cylinder.sorted(Carr);
+        for (Cylinder c: sorted) {
+            System.out.println(c.volume());
         }
 
+
+
+
+    }
+
+    static Cylinder[] merge(Cylinder[] A, Cylinder[] B) {
+
+        int index_A = 0;
+        int index_B = 0;
+        int index_merged = 0;
+
+        Cylinder[] merged = new Cylinder[A.length + B.length];
+
+        while ( index_A < A.length || index_B < B.length) {
+
+            if (index_A >= A.length) {
+                merged[index_merged] = B[index_B];
+                index_merged++;
+                index_B++;
+            } else if (index_B >= B.length) {
+                merged[index_merged] = A[index_A];
+                index_merged++;
+                index_A++;
+            } else if (A[index_A].volume() < B[index_B].volume()) {
+                merged[index_merged] = A[index_A];
+                index_merged++;
+                index_A++;
+            } else {
+                merged[index_merged] = B[index_B];
+                index_merged++;
+                index_B++;
+            }
+        }
+
+        return merged;
     }
 }
